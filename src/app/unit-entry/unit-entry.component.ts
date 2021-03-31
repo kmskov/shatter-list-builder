6dfe9cca760afc7319873601eb55c9bf943265c9
kmskov <kms5@njit.edu>
2021-03-30 23:13:03 -0400

    display unit entry details, added tracking for weapons

--- src/app/unit-entry/unit-entry.component.ts
+++ src/app/unit-entry/unit-entry.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as factions from '../factions.json';
+import * as reference from '../reference.json';
 
 @Component({
   selector: 'app-unit-entry',
@@ -7,11 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnitEntryComponent implements OnInit {
 
-  unitEntry = 'unitEntry';
+  unitEntry: any;
+  weaponIds: string[] = [];
+  weaponEntries: any[] = [];
+  currentWeaponNames: string[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.unitEntry = factions.vlast.hq[0];
+
+    this.unitEntry.weapons.forEach(weapon => {
+      this.weaponIds.push(weapon);
+    });
+    this.unitEntry.upgrades.forEach(upgradeEntry => {
+      if (upgradeEntry.hasOwnProperty('weapon')) {
+        this.weaponIds.push(upgradeEntry.weapon);
+      }
+    });
+
+    this.weaponIds.forEach(weaponId => {
+      let weaponEntry = reference.weapons.find(i => i.id === weaponId);
+      this.weaponEntries.push(weaponEntry);
+
+      if ( this.unitEntry.weapons.indexOf(weaponEntry.id) > -1) {
+        this.currentWeaponNames.push(weaponEntry.name);
+      }
+    });
+
+    console.log('unit-entry.ngOnInit finished');
   }
 
 }
